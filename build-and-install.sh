dcc9e3774e135976cfc3fcfd44537e3a62a6b9cd
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2026-02-09 11:52:22 +0000

    v1.10.0

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/TheAppgineer/flatpak-flutter 2> /dev/null
 mkdir log 2> /dev/null
 touch log/untranslated-messages.txt
+set -euo pipefail
 python flatpak-flutter/flatpak-flutter.py ./flatpak-flutter.yml
 flatpak run org.flatpak.Builder build --user --install-deps-from=flathub --force-clean --ccache --sandbox --install io.github.glitterware.Passy.yml
 rm -rf flatpak-flutter
